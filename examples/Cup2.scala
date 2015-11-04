f0451b71e8d446321c6f754d585d82a70a418926
brenteritou <brent.eritou@boldradius.com>
2015-11-04 10:53:11 -0500

    Implement Cup5 - variable argument list

--- examples/Cup2.scala
+++ examples/Cup2.scala
@@ -1,9 +1,7 @@
 // Cup2.scala
 import com.atomicscala.AtomicTest._
 
-class Cup2 {
-  var percentFull = 0
-  val max = 100
+class Cup2 (var percentFull: Int = 0, max:Int = 100) {
   def add(increase:Int):Int = {
     percentFull += increase
     if(percentFull > max) {
@@ -65,3 +63,21 @@ cup3.percentFull is 56
 val cup4 = new Cup4
 cup4.set(56)
 cup4.get() is 56
+
+class Cup5 (var percentFull: Int = 0) {
+  var max:Int = 100
+  def increase(adj:Int*):Int = {
+    for (a <- adj) {
+      percentFull += a
+
+      if (percentFull > max) {
+        percentFull = max
+
+      }
+    }
+    percentFull // Return this value
+  }
+}
+val cup5 = new Cup5(0)
+cup5.increase(20, 30, 50, 20, 10, -10, -40, 10, 50) is 100
+cup5.increase(10, 10, -10, 10, 90, 70, -70) is 30
