b42fd3380574f66531da0eeb98b73c81cf830602
brenteritou <brent.eritou@boldradius.com>
2015-11-04 10:10:06 -0500

    Implement Family

--- examples/ClassArg.scala
+++ examples/ClassArg.scala
@@ -9,3 +9,16 @@ class ClassArg(a:Int) {
 val ca = new ClassArg(19)
 ca.f() is 190
 // ca.a // error
+
+class Family(names:String*) {
+  def familySize(): Int = {
+    names.length
+  }
+}
+
+val family1 = new Family("Mom",
+  "Dad", "Sally", "Dick")
+family1.familySize() is 4
+val family2 =
+  new Family("Dad", "Mom", "Harry")
+family2.familySize() is 3
