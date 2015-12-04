9d8710388fbf1d474979d60542bbfc7041e281a9
brenteritou <brent.eritou@boldradius.com>
2015-12-03 19:28:12 -0500

    Constructor exercises

--- examples/Coffee.scala
+++ examples/Coffee.scala
@@ -2,7 +2,7 @@
 import com.atomicscala.AtomicTest._
 
 class Coffee(val shots:Int = 2,
-             val decaf:Boolean = false,
+             val decaf:Int = 0,
              val milk:Boolean = false,
              val toGo:Boolean = false,
              val syrup:String = "") {
@@ -16,7 +16,7 @@ class Coffee(val shots:Int = 2,
   }
   def pourShots():Unit = {
     for(s <- 0 until shots)
-      if(decaf)
+      if(decaf > 0)
         result += "decaf shot "
       else
         result += "shot "
@@ -28,6 +28,7 @@ class Coffee(val shots:Int = 2,
   def addSyrup():Unit = {
     result += syrup
   }
+  val caf = shots - decaf
   getCup()
   pourShots()
   addMilk()
@@ -36,7 +37,52 @@ class Coffee(val shots:Int = 2,
 
 val usual = new Coffee
 usual.result is "HereCup shot shot "
-val mocha = new Coffee(decaf = true,
+val mocha = new Coffee(decaf = 1,
   toGo = true, syrup = "Chocolate")
-mocha.result is
-"ToGoCup decaf shot decaf shot Chocolate"
+mocha.result is "ToGoCup decaf shot decaf shot Chocolate"
+
+val doubleHalfCaf =
+  new Coffee(shots=2, decaf=1)
+val tripleHalfCaf =
+  new Coffee(shots=3, decaf=2)
+doubleHalfCaf.decaf is 1
+doubleHalfCaf.caf is 1
+doubleHalfCaf.shots is 2
+tripleHalfCaf.decaf is 2
+tripleHalfCaf.caf is 1
+tripleHalfCaf.shots is 3
+
+class Tea(decaf:Boolean = false,
+          name:String = "Earl Grey",
+          sugar:Boolean = false,
+          milk:Boolean = false ) {
+  def describe() = {
+    if(decaf)
+      name + " decaf"
+    else if (milk && sugar)
+      name + " + milk + sugar"
+    else if (sugar)
+      name + " + sugar"
+    else name
+  }
+  def calories() = if(sugar && milk) 116 else if (sugar) 16 else 0
+}
+
+val tea = new Tea
+tea.describe is "Earl Grey"
+tea.calories is 0
+
+val lemonZinger = new Tea(
+  decaf = true, name="Lemon Zinger")
+lemonZinger.describe is "Lemon Zinger decaf"
+lemonZinger.calories is 0
+
+val sweetGreen = new Tea(
+  name="Jasmine Green", sugar=true)
+sweetGreen.describe is "Jasmine Green + sugar"
+sweetGreen.calories is 16
+
+val teaLatte = new Tea(
+  sugar=true, milk=true)
+teaLatte.describe is "Earl Grey + milk + sugar"
+teaLatte.calories is 116
