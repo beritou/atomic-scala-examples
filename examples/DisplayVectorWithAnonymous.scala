bdd416faa4bf16fd9f16c451397da522fa66c083
brenteritou <brent.eritou@boldradius.com>
2015-12-06 14:48:22 -0500

    First 3 function object exercises

--- examples/DisplayVectorWithAnonymous.scala
+++ examples/DisplayVectorWithAnonymous.scala
@@ -1,4 +1,18 @@
 // DisplayVectorWithAnonymous.scala
 
+import com.atomicscala.AtomicTest._
+
 val v = Vector(1, 2, 3, 4)
 v.foreach(n => println("> " + n))
+
+var str = ""
+v.foreach(n => str = str + n)
+str is "1234"
+
+str = ""
+v.foreach(n => str = str + n + ",")
+str is "1,2,3,4,"
+
+val dogYears = (y:Int) => (y * 7)
+
+dogYears(10) is 70
