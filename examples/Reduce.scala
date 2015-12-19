d9dd7ec79c5dc1362ded7c61472a2ae9a17b4279
brenteritou <brent.eritou@boldradius.com>
2015-12-19 14:32:07 -0500

    Reduce in a for loop

--- examples/Reduce.scala
+++ examples/Reduce.scala
@@ -3,3 +3,18 @@ import com.atomicscala.AtomicTest._
 
 val v = Vector(1, 10, 100, 1000)
 v.reduce((sum, n) => sum + n) is 1111
+
+var result = 0
+for (n <- v)
+  result += n
+result is 1111
+
+def sumIt(numbers:Int*) = {
+  numbers.reduce((x,y) => x + y)
+}
+sumIt(1, 2, 3) is 6
+sumIt(45, 45, 45, 60) is 195
+
+val sumItBetter = (numbers: Seq[Int]) => numbers.reduce((x,y) => x + y)
+sumItBetter(Seq(1, 2, 3)) is 6
+sumItBetter(Seq(45, 45, 45, 60)) is 195
