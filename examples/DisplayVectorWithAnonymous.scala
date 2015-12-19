51fff1b9de8e81de62c973c83f44921a692210bd
brenteritou <brent.eritou@boldradius.com>
2015-12-19 12:58:09 -0500

    String interpolation with expressions

--- examples/DisplayVectorWithAnonymous.scala
+++ examples/DisplayVectorWithAnonymous.scala
@@ -16,3 +16,15 @@ str is "1,2,3,4,"
 val dogYears = (y:Int) => (y * 7)
 
 dogYears(10) is 70
+
+
+
+var s = ""
+val v2 = Vector(1, 5, 7, 8)
+v2.foreach(n => s = s"$s${dogYears(n)} ")
+s is "7 35 49 56 "
+
+var s3 = ""
+val v3 = Vector(1, 5, 7, 8)
+v3.foreach(n => s3 = s"$s3${n * 7} ")
+s3 is "7 35 49 56 "
