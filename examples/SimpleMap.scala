6a04fa9bed8b87bd49e1bd1b9b40fba75e029741
brenteritou <brent.eritou@boldradius.com>
2015-12-19 14:21:41 -0500

    Map vs foreach vs for loop

--- examples/SimpleMap.scala
+++ examples/SimpleMap.scala
@@ -1,5 +1,25 @@
 // SimpleMap.scala
 import com.atomicscala.AtomicTest._
 
+import scala.collection.immutable.VectorBuilder
+
 val v = Vector(1, 2, 3, 4)
 v.map(n => n + 1) is Vector(2, 3, 4, 5)
+
+val v2 = Vector(1, 2, 3, 4)
+v2.map(n => n * 11 + 10) is Vector(21, 32, 43, 54)
+
+val v3 = Vector(1, 2, 3, 4)
+var vec = Vector[Int]()
+//no worky
+//v3.foreach(n => vec = vec ++ Vector(n * 11 + 10)) is Vector(21, 32, 43, 54)
+
+val v4 = Vector(1, 2, 3, 4)
+var vecbuild = new VectorBuilder[Int]
+for (n <- v4)
+  vecbuild += n * 11 + 10
+
+vecbuild.result is Vector(21, 32, 43, 54)
+
+
+
