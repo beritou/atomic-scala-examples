23054506c414b820dd7e441d8d7cbfeb9f668e2e
brenteritou <brent.eritou@boldradius.com>
2015-12-23 22:10:31 -0500

    Pluralize strings via pattern match

--- examples/PatternMatchingWithTypes.scala
+++ examples/PatternMatchingWithTypes.scala
@@ -11,14 +11,23 @@ def acceptAnything(x:Any):String = {
     case _ => "I don't know what that is!"
   }
 }
-acceptAnything(5) is
-  "An Int Less than 20: 5"
+acceptAnything(5) is "An Int Less than 20: 5"
 acceptAnything(25) is "Some Other Int: 25"
-acceptAnything("Some text") is
-"A String: Some text"
+acceptAnything("Some text") is "A String: Some text"
 
 case class Person(name:String)
 val bob = Person("Bob")
 acceptAnything(bob) is "A person Bob"
-acceptAnything(Vector(1, 2, 5)) is
-"I don't know what that is!"
+acceptAnything(Vector(1, 2, 5)) is "I don't know what that is!"
+
+def plus1(word: Any) =
+  word match {
+    case s:String => s + "s"
+    case i:Int => i + 1
+    case p:Person => p + " + guest"
+    case _ => "WTF"
+  }
+
+plus1("car") is "cars"
+plus1(67) is 68
+plus1(Person("Joanna")) is "Person(Joanna) + guest"
